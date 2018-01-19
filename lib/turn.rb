e1146e4c60fc73063c442e779c4454bacb9be8b7
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-19 02:43:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,8 +36,10 @@ def turn(board)
   input = input_to_index(input)
   if input.between?(0, 8) && valid_move?(board, input)
     move(board, input)
+    display_board(board)
   else
     turn(board)
   end
+  
 
 end
