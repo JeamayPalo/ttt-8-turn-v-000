d0c2cab9b8d6374e66ac21b594a996ca22b18a1f
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-19 02:38:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,6 @@ def turn(board)
   input = gets.strip
   input = input_to_index(input)
   if input.between?(1, 9) && !valid_move?(board, input)
-    move (board, index)
+    move(board, index)
   end
-
 end
